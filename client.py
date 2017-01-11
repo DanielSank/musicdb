60aa4b54dece8c31db98dc22a9607ea584f077b5
Daniel Sank <sank.daniel@gmail.com>
2017-01-11 01:35:14 -0800

    Simplify client.py
    - Add url generator.
    - Remove entity creation/fetching as we should factor that out.

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 
 
+import os
+
+
 import mutagen
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
@@ -11,54 +14,27 @@ import yaml
 import musicdb.models as models
 
 
-try:
-    import musicdb.config as config
-except ImportError as e:
-    message = e.args[0]
-    message = message + ("\nFollow instructions in setup.md to "
-                         "create config.py.")
-    e.args = (message,)
-    raise
-
-engine = sa.create_engine(config.config['DB_URL'], echo=False)
-Sessionmaker = orm.sessionmaker(bind=engine)
-
-
 # UTILITY
 
-def get_or_create(session, model, get_params, create_params=None):
-    """Get a or create an instance in the database.
-
-    Args:
-        session:
-        model: Class of the object to create.
-        get_params (dict): parameters needed to uniquely identify an already
-            existing entity.
-        create_params (dict): Additional parameters needed if the entity does
-            not already exist and has to be created
-
-    Returns:
-        an ORM object representing the instance.
-        (bool): True if the instance was created, False if it already existed.
-    """
-    if create_params is None:
-        create_params = {}
-    try:
-        instance = session.query(model).filter_by(**get_params).one()
-    except NoResultFound:
-        instance = None
-    if instance:
-        created = False
-    else:
-        all_params = dict(get_params.items() + create_params.items())
-        instance = model(**all_params)
-        session.add(instance)
-        created = True
-    return instance, created
+def get_url(role=None):
+    if role is None:
+        role = os.environ['MUSICDB_ROLE']
+    home = os.path.expanduser('~')
+    config_file_name = os.path.join(home, ".musicdb_config.yml")
+    with open(config_file_name, 'r') as stream:
+        parameters = yaml.load(stream)[role]
+    url = r'mysql+mysqldb://{}:{}@{}/{}'.format(
+            parameters['USERNAME'],
+            parameters['PASSWORD'],
+            parameters['HOST'],
+            parameters['DBNAME'])
+    return url
 
 
-def get_one(session, model, params):
-    return session.query(model).filter_by(**params).one()
+def make_session(role):
+    url = get_url(role)
+    engine = sa.create_engine(url, echo=False)
+    return orm.sessionmaker(bind=engine)()
 
 
 def make_yaml_from_file(filepath):
